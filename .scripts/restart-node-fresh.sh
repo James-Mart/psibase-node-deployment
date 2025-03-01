7013109f1df8267b63f55ad77fe27ca4d72a8936
James Mart <jmartx93@gmail.com>
2025-03-01 21:28:13 +0000

    fix bug

--- .scripts/restart-node-fresh.sh
+++ .scripts/restart-node-fresh.sh
@@ -5,7 +5,7 @@ echo "Bringing down all containers..."
 docker compose down --remove-orphans
 
 echo "Removing psinode volume..."
-docker volume rm psibase-node_psinode-volume || echo "[Warning] Volume doesn't exist or cannot be removed"
+docker volume rm psibase-node-deployment_psinode-volume || echo "[Warning] Volume doesn't exist or cannot be removed"
 
 echo "Starting services with clean volume..."
 docker compose up -d
